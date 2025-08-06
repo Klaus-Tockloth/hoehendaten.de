19c756639567e151c3b49d7218b044712b9a96a7
Klaus Tockloth <klaus.tockloth@googlemail.com>
2025-08-06 14:19:42 +0200

    Webseite erweitert

--- tidy-check-all.sh
+++ tidy-check-all.sh
@@ -9,6 +9,8 @@
 # - v1.1.0 - 2025/06/13 website added
 # - v1.2.0 - 2025/07/12 websites added
 # - v1.3.0 - 2025/07/31 websites added
+# - v1.4.0 - 2025/08/04 websites added
+# - v1.5.0 - 2025/08/06 websites added
 #
 # Prerequisites:
 # - tidy-html5 installed
@@ -20,14 +22,18 @@ set -o verbose
 tidy -version
 
 tidy -errors -quiet abbildung-allgemein.html
+tidy -errors -quiet abbildung-farbrelief.html
 tidy -errors -quiet abbildung-gelaenderauheit.html
 tidy -errors -quiet abbildung-hangexposition.html
 tidy -errors -quiet abbildung-hangneigung.html
 tidy -errors -quiet abbildung-hoehenschichtlinien.html
 tidy -errors -quiet abbildung-kolorierung.html
 tidy -errors -quiet abbildung-schummerung.html
+tidy -errors -quiet abbildung-tpi.html
+tidy -errors -quiet abbildung-tri.html
 tidy -errors -quiet api-allgemein.html
 tidy -errors -quiet api-aspectrequest.html
+tidy -errors -quiet api-colorreliefrequest.html
 tidy -errors -quiet api-contoursrequest.html
 tidy -errors -quiet api-gpxrequest.html
 tidy -errors -quiet api-gpxanalyzerequest.html
@@ -36,7 +42,10 @@ tidy -errors -quiet api-pointrequest.html
 tidy -errors -quiet api-rawtifrequest.html
 tidy -errors -quiet api-roughnessrequest.html
 tidy -errors -quiet api-sloperequest.html
+tidy -errors -quiet api-tpirequest.html
+tidy -errors -quiet api-trirequest.html
 tidy -errors -quiet api-utmpointrequest.html
+tidy -errors -quiet dienst-farbrelief.html
 tidy -errors -quiet dienst-gelaenderauheit.html
 tidy -errors -quiet dienst-gpx.html
 tidy -errors -quiet dienst-gpx-analyse.html
@@ -48,6 +57,8 @@ tidy -errors -quiet dienst-punkt_utm.html
 tidy -errors -quiet dienst-punkt.html
 tidy -errors -quiet dienst-quelldaten.html
 tidy -errors -quiet dienst-schummerung.html
+tidy -errors -quiet dienst-tpi.html
+tidy -errors -quiet dienst-tri.html
 tidy -errors -quiet grundlagen.html
 tidy -errors -quiet impressum.html
 tidy -errors -quiet index.html
--- tidy-format-all.sh
+++ tidy-format-all.sh
@@ -9,6 +9,8 @@
 # - v1.1.0 - 2025/06/13 website added
 # - v1.2.0 - 2025/07/12 websites added
 # - v1.3.0 - 2025/07/31 websites added
+# - v1.4.0 - 2025/08/04 websites added
+# - v1.5.0 - 2025/08/06 websites added
 #
 # Prerequisites:
 # - tidy-html5 installed
@@ -20,14 +22,18 @@ set -o verbose
 tidy -version
 
 tidy -config tidy-config.txt -quiet -modify abbildung-allgemein.html
+tidy -config tidy-config.txt -quiet -modify abbildung-farbrelief.html
 tidy -config tidy-config.txt -quiet -modify abbildung-gelaenderauheit.html
 tidy -config tidy-config.txt -quiet -modify abbildung-hangexposition.html
 tidy -config tidy-config.txt -quiet -modify abbildung-hangneigung.html
 tidy -config tidy-config.txt -quiet -modify abbildung-hoehenschichtlinien.html
 tidy -config tidy-config.txt -quiet -modify abbildung-kolorierung.html
 tidy -config tidy-config.txt -quiet -modify abbildung-schummerung.html
+tidy -config tidy-config.txt -quiet -modify abbildung-tpi.html
+tidy -config tidy-config.txt -quiet -modify abbildung-tri.html
 tidy -config tidy-config.txt -quiet -modify api-allgemein.html
 tidy -config tidy-config.txt -quiet -modify api-aspectrequest.html
+tidy -config tidy-config.txt -quiet -modify api-colorreliefrequest.html
 tidy -config tidy-config.txt -quiet -modify api-contoursrequest.html
 tidy -config tidy-config.txt -quiet -modify api-gpxrequest.html
 tidy -config tidy-config.txt -quiet -modify api-gpxanalyzerequest.html
@@ -36,7 +42,10 @@ tidy -config tidy-config.txt -quiet -modify api-pointrequest.html
 tidy -config tidy-config.txt -quiet -modify api-rawtifrequest.html
 tidy -config tidy-config.txt -quiet -modify api-roughnessrequest.html
 tidy -config tidy-config.txt -quiet -modify api-sloperequest.html
+tidy -config tidy-config.txt -quiet -modify api-tpirequest.html
+tidy -config tidy-config.txt -quiet -modify api-trirequest.html
 tidy -config tidy-config.txt -quiet -modify api-utmpointrequest.html
+tidy -config tidy-config.txt -quiet -modify dienst-farbrelief.html
 tidy -config tidy-config.txt -quiet -modify dienst-gelaenderauheit.html
 tidy -config tidy-config.txt -quiet -modify dienst-gpx.html
 tidy -config tidy-config.txt -quiet -modify dienst-gpx-analyse.html
@@ -48,6 +57,8 @@ tidy -config tidy-config.txt -quiet -modify dienst-punkt_utm.html
 tidy -config tidy-config.txt -quiet -modify dienst-punkt.html
 tidy -config tidy-config.txt -quiet -modify dienst-quelldaten.html
 tidy -config tidy-config.txt -quiet -modify dienst-schummerung.html
+tidy -config tidy-config.txt -quiet -modify dienst-tpi.html
+tidy -config tidy-config.txt -quiet -modify dienst-tri.html
 tidy -config tidy-config.txt -quiet -modify grundlagen.html
 tidy -config tidy-config.txt -quiet -modify impressum.html
 tidy -config tidy-config.txt -quiet -modify index.html
